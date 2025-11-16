2150bf0e651c4d10fe8c69bd1cc865658a3bf5c1
Bartłomiej Ciupak <153275462+bciupak@users.noreply.github.com>
2025-11-16 20:08:14 +0100

    Refactor DataGenerator to use String arrays

--- CW_4/AiSD2025ZEx4/src/test/java/pl/edu/pw/ee/aisd2025zex4/performance/DataGenerator.java
+++ CW_4/AiSD2025ZEx4/src/test/java/pl/edu/pw/ee/aisd2025zex4/performance/DataGenerator.java
@@ -3,43 +3,42 @@ package pl.edu.pw.ee.aisd2025zex4.performance;
 import java.util.Random;
 
 public class DataGenerator{
-
-    public static Double[] createRandomData(int size) {
+    public static String[] generateRandData(int size) {
         assert size >= 0;
 
-        Double[] nums = new Double[size];
+        String[] nums = new String[size];
 
         long eliteSeed = 31337;
         Random rand = new Random(eliteSeed);
 
         for (int i = 0; i < size; i++) {
-            nums[i] = rand.nextDouble();
+            nums[i] = Double.toString(rand.nextDouble());
         }
 
         return nums;
     }
 
-    public static Double[] createAscendingData(int size) {
+    public static String[] generateAscData(int size) {
         assert size >= 0;
 
-        Double[] nums = new Double[size];
+        String[] nums = new String[size];
         double start = 100_000_000;
 
         for (int i = 0; i < size; i++) {
-            nums[i] = start + i;
+            nums[i] = Double.toString(start + i);
         }
 
         return nums;
     }
 
-    public static Double[] createDescendingData(int size) {
+    public static String[] generateDescData(int size) {
         assert size >= 0;
 
-        Double[] nums = new Double[size];
+        String[] nums = new String[size];
         double start = 100_000_000;
 
         for (int i = 0; i < size; i++) {
-            nums[i] = start + (size - 1 - i);
+            nums[i] = Double.toString(start + (size - 1 - i));
         }
 
         return nums;
