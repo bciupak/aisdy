b77f2a96b744f2daa4baa4ffa70c9249183fd82c
Bartłomiej Ciupak <153275462+bciupak@users.noreply.github.com>
2026-07-09 12:13:13 +0200

    Delete CW_4/AiSD2025ZEx4 directory

--- CW_4/AiSD2025ZEx4/src/main/java/pl/edu/pw/ee/aisd2025zex4/Color.java
+++
@@ -1,5 +0,0 @@
-package pl.edu.pw.ee.aisd2025zex4;
-
-public enum Color {
-    RED, BLACK;
-}
--- CW_4/AiSD2025ZEx4/src/main/java/pl/edu/pw/ee/aisd2025zex4/exception/NotYetImplementedException.java
+++
@@ -1,9 +0,0 @@
-package pl.edu.pw.ee.aisd2025zex4.exception;
-
-public class NotYetImplementedException extends RuntimeException {
-
-    public NotYetImplementedException(String message) {
-        super(message);
-    }
-
-}
